cc85affaf8857498fdca03e5ac27eeb1bb19145c
abiesheck2005 <abiesheck1405@gmail.com>
2026-09-01 12:45:57 +0530

    hello

--- sam.py
+++ sam.py
@@ -2,5 +2,9 @@ print("welcome to git hub ")
 print('topic-branch')
 
 
-print("git init-untracking")
-print("git add .- add all file)
+print("git init|-untracking in file")
+print("git add .|- add all file")
+print("git commit -m| ->('message')")
+print("git branch -m| main->'modify'master to main")  
+print("git remote add origin| repo link")
+print("git push -u origin main|pushing to link")    
